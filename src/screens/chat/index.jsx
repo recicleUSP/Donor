8be6663c8af50f893cf273f444f46e592f67b188
leonardo8787 <leocampos8787@gmail.com>
2023-07-18 12:09:25 -0300

    atualiza chat

--- src/screens/chat/index.jsx
+++ src/screens/chat/index.jsx
@@ -1,55 +1,70 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { StyleSheet, View, TextInput, FlatList, TouchableOpacity, Text, Image } from 'react-native';
-// import { getFirestore, collection, onSnapshot, addDoc, firebaseApp, firebase } from "firebase/firestore";
-import { setDoc, getDoc, collection, onSnapshot, addDoc, getFirestore, firebaseApp, Firestore } from "firebase/firestore";
+import { collection, onSnapshot, addDoc, getFirestore, firebaseApp, Firestore, Timestamp } from "firebase/firestore";
 import { Colors, Theme } from "../../constants/setting";
 import { ImageCircleIcon } from "../../components/images";
-import { ContainerTopClean } from "../../components/containers";
+import { DonorContext } from '../../contexts/donor/context';
 import { SizedBox } from 'sizedbox';
-import { DarkTheme } from '@react-navigation/native';
-import { DonorContext } from "../../contexts/donor/context";
 
 const firestore = getFirestore(firebaseApp);
 
+const formatTimestamp = (timestamp) => {
+  if (!timestamp) return '';
+  const date = timestamp.toDate();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  return `${hours}:${minutes}`;
+};
 
 export function ChatScreen() {
-  const {donorState, donorDispach} = useContext(DonorContext)
-  const basedImage                       = require("../../../assets/images/profile.webp");
-  const [image, setImage]                = useState(basedImage);
+  const { donorState, donorDispach } = useContext(DonorContext);
+  const [image, setImage] = useState(require("../../../assets/images/profile.webp"));
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  // const [senderPhoto, setSenderPhoto] = useState(null);
 
   useEffect(() => {
     const messagesRef = collection(firestore, 'messages');
     const unsubscribe = onSnapshot(messagesRef, (snapshot) => {
       const messageList = snapshot.docs
-        .map((doc) => doc.data())
-        .filter((message) => message.recipientId === donorState.name); // Filtra as mensagens com base no ID do remetente
+        .map((doc) => ({ ...doc.data(), id: doc.id }))
+        .filter((message) => message.senderId === donorState.id || message.recipientId === donorState.id)
+        .sort((a, b) => {
+          // Check if both messages have a timestamp
+          if (a.timestamp && b.timestamp) {
+            return a.timestamp.toMillis() - b.timestamp.toMillis();
+          } else if (a.timestamp) {
+            return -1; // Place message without timestamp at the end (newest)
+          } else if (b.timestamp) {
+            return 1; // Place message without timestamp at the end (newest)
+          } else {
+            return 0; // If both messages don't have a timestamp, maintain their order
+          }
+        });
+  
       setMessages(messageList);
     });
   
     return () => unsubscribe();
   }, []);
+  
 
   useEffect(() => {
-    setImage(donorState.photoUrl
-      ? { uri: donorState.photoUrl }
-      : basedImage);
+    if (donorState.photoUrl) {
+      setImage({ uri: donorState.photoUrl });
+    }
   }, [donorState.photoUrl]);
 
   async function changeProfileImage() {
-    let result = await ImagePicker.launchImageLibraryAsync({
+    const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [5, 5],
+      aspect: [1, 1],
       quality: 1,
     });
 
-    if (!result.canceled) {
-      const source = { uri: result.assets[0].uri };
+    if (!result.cancelled) {
+      const source = { uri: result.uri };
       setImage(source);
-      setLoandding(true);
       donorDispach({ type: Types.LOADIMAGE, uri: source.uri, cb: changeImageCB });
     }
   }
@@ -68,13 +83,13 @@ export function ChatScreen() {
       return;
     }
 
-    const user = donorState.name;
+    const user = donorState.id;
 
     const newMessage = {
       text: message,
       senderId: user,
-      recipientId: 'Leonardo coleta',
-      // timestamp: serverTimestamp() // Obtém o timestamp do servidor Firebase
+      recipientId: '5fIkyWhXvJVF15w2GlhYJMDiIY43',
+      timestamp: Timestamp.fromDate(new Date()), // Add the current timestamp using Firestore Timestamp object
     };
 
     await addDoc(collection(firestore, 'messages'), newMessage);
@@ -91,6 +106,7 @@ export function ChatScreen() {
         {item.senderPhotoUrl && <Image source={{ uri: item.senderPhotoUrl }} style={styles.senderPhoto} />}
         <View style={styles.messageContent}>
           <Text style={styles.messageText}>{item.text}</Text>
+          <Text style={styles.timestampText}>{formatTimestamp(item.timestamp)}</Text>
         </View>
       </View>
     );
@@ -98,25 +114,23 @@ export function ChatScreen() {
 
   return (
     <View style={styles.container}>
-      <ImageCircleIcon
-        size={130}
-        sizeIcon={0}
-        align={"flex-start"}
-        img={image}
-        color={Colors[Theme][5]}
-        bgColor={Colors[Theme][0]}
-      />
-      <ContainerTopClean
-        fun={() => { }}
-        text={"   " + donorState.name}
-        icon="information"
-      />
-      <SizedBox vertical={35} />
+      <TouchableOpacity onPress={changeProfileImage}>
+        <ImageCircleIcon
+          size={130}
+          sizeIcon={30}
+          align={"flex-start"}
+          img={image}
+          color={Colors[Theme][5]}
+          bgColor={Colors[Theme][0]}
+        />
+      </TouchableOpacity>
+      <SizedBox vertical={20} />
       <FlatList
         data={messages}
         renderItem={renderMessage}
         keyExtractor={(item) => item.id}
-        contentContainerStyle={[styles.messagesContainer]}
+        contentContainerStyle={styles.messagesContainer}
+        inverted // Display the latest messages at the bottom
       />
       <View style={styles.inputContainer}>
         <TextInput
@@ -124,6 +138,7 @@ export function ChatScreen() {
           placeholder="Digite uma mensagem..."
           value={message}
           onChangeText={(text) => setMessage(text)}
+          placeholderTextColor="#8C8C8C"
         />
         <TouchableOpacity style={styles.button} onPress={handleMessageSend}>
           <Text style={styles.buttonText}>Enviar</Text>
@@ -136,25 +151,29 @@ export function ChatScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
     backgroundColor: '#F5F5F5',
+    padding: 16,
   },
   messagesContainer: {
-    paddingVertical: 16,
-    paddingHorizontal: 10,
+    paddingTop: 8,
+    paddingBottom: 16,
   },
   messageContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    borderRadius: 8,
-    padding: 8,
+    alignItems: 'flex-start', // Align text to the start (top) of the message container
+    maxWidth: '70%', // Limit the message container to 70% of the width
+    borderRadius: 20,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
     marginBottom: 8,
   },
   currentUserMessageContainer: {
-    backgroundColor: '#10b981', // Cor para mensagens enviadas pelo usuário atual
+    backgroundColor: '#5FC9A7', // Light green for messages sent by the current user
+    alignSelf: 'flex-end',
   },
   otherUserMessageContainer: {
-    backgroundColor: '#FF0000', // Cor para mensagens recebidas de outros usuários
+    backgroundColor: '#F5A623', // Orange for messages received from other users
+    alignSelf: 'flex-start',
   },
   senderPhoto: {
     width: 32,
@@ -167,35 +186,35 @@ const styles = StyleSheet.create({
   },
   messageText: {
     fontSize: 16,
-    color: "#fafffd",
+    color: "#FFFFFF",
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
     paddingHorizontal: 12,
-    paddingBottom: 8,
+    paddingVertical: 8,
+    marginBottom: 8,
   },
   input: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 8,
-    paddingHorizontal: 8,
-    marginRight: 8,
+    fontSize: 16,
+    color: '#333333',
+    marginLeft: 8,
   },
   button: {
     backgroundColor: "#10b981",
-    borderRadius: 8,
-    paddingVertical: 8,
-    paddingHorizontal: 12,
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
   },
   buttonText: {
     color: '#FFFFFF',
     fontSize: 16,
-  },
-  containerTransparent: {
-    flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Cor preta com 50% de transparência
+    fontWeight: 'bold',
   },
 });
 
 export default ChatScreen;
+
